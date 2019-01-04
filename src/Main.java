1884208ccde0a6f68212ca516e331f8927c7e429
matias erenchun <matiaserenchun@gail.com>
2019-01-04 16:00:38 -0300

    correccion de error del args[0]

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main
 
             public static void main(String[] args)
             {
-                String aux = args[1];
+                String aux = args[0];
                 Integer n = Integer.parseInt(aux);
                 miDeus = new Deus(n);
 
